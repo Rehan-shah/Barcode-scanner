66f15c4087752bd0e9c82729dcecef5c9c6dcc0b
Rehan-shah <Rehansnehalshah1@gmail.com>
2023-11-18 09:26:39 +0530

    Initial Commit

---
+++ Barcode scanner/Barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_scannerApp.swift
+//  Barcode scanner
+//
+//  Created by Rehan Shah on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
